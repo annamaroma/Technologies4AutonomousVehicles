0c382879a3b2c7a601707ef71611fe1a32b9edaa
Anna Roma <s285951@studenti.polito.it>
2026-02-24 10:51:00 +0100

    iterazione sulle due città da finire A*

---
+++ assignment1/Astar.py
@@ -0,0 +1,5 @@
+#implement A* algorithm n three diMerent variants corresponding to three diMerent heuristic functions:
+# a. Manhattan distance: ℎ(𝑛) = |𝑥1− 𝑥2| + |𝑦1− 𝑦2| 
+# b. Euclidean distance: ℎ(𝑛) = √((𝑥1− 𝑥2)² + (𝑦1− 𝑦2)²) 
+# c. Haversine distance. It computes the great-circle distance between two points on a sphere given their latitude (∅) and longitude (𝜆) in radians.
+
